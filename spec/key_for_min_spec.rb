3e98e4ce828ee9652418029ed2ac22d1e2dddd68
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-18 15:17:21 +0000

    Automatically backed up by Learn

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -62,4 +62,12 @@ describe "smallest hash value" do
 
 end
 
-def key_for_min_value
+def key_for_min_value(name_hash)
+  empty_hash = nil
+   name_hash.collect do |name, value|
+     if value <= 1
+       return name
+   end
+  end
+  empty_hash
+end
